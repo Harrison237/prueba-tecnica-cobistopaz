17fefe6c7f483c0dad6495bdc39e75194a5f55a4
Harrison Ramirez <harrisonramirez237@gmail.com>
2024-07-02 11:44:38 -0500

    Corrección
    Otra corrección al script de base de datos

--- Tabla_Usuarios.sql
+++ Tabla_Usuarios.sql
@@ -3,8 +3,8 @@ create database cobisTopazPrueba;
 use cobisTopazPrueba;
 
 create table user_tabla (
-    id varchar(255) not null primary key unique,
-    nombre_usuario varchar(255) not null,
+    id varchar(255) not null primary key,
+    nombre_usuario varchar(255) not null unique,
     contrasena text not null,
     roles text not null
 )
