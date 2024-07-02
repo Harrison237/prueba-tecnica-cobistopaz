477b9e0929c678b411f910d7ee1635b0fb71e06b
Harrison Ramirez <harrisonramirez237@gmail.com>
2024-07-02 11:41:48 -0500

    Corrección
    Corrección en tipo de dato contraseña para evitar errores

--- Tabla_Usuarios.sql
+++ Tabla_Usuarios.sql
@@ -5,6 +5,6 @@ use cobisTopazPrueba;
 create table user_tabla (
     id varchar(255) not null primary key unique,
     nombre_usuario varchar(255) not null,
-    contrasena varchar(40) not null,
+    contrasena text not null,
     roles text not null
 )
