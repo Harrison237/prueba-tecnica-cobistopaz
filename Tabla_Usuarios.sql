f5119c87e23f783592fd05efa1f5e67a098d0310
Harrison Ramirez <harrisonramirez237@gmail.com>
2024-07-02 12:40:46 -0500

    finalización
    Archivo Readme y ajustes finales del proyecto

--- Tabla_Usuarios.sql
+++ Tabla_Usuarios.sql
@@ -2,9 +2,4 @@ create database cobisTopazPrueba;
 
 use cobisTopazPrueba;
 
-create table user_tabla (
-    id varchar(255) not null primary key,
-    nombre_usuario varchar(255) not null unique,
-    contrasena text not null,
-    roles text not null
-)
+create table user_tabla (id varchar(255) not null primary key, nombre_usuario varchar(255) not null unique, contrasena text not null, roles text not null );
